0a0bbad6467f64e314c99c223e62520a91b4cc20
alswns <49472611+alswns@users.noreply.github.com>
2020-02-02 22:03:41 +0900

    http

--- src/apis/Oauth.js
+++ src/apis/Oauth.js
@@ -1,3 +1,3 @@
 export function Facebook(){
-    window.location.href='https://www.facebook.com/v5.0/dialog/oauth?client_id=186645249405501&redirect_uri=http://itagency119.com/login&state=&response_type=token'
+    window.location.href='https://www.facebook.com/v5.0/dialog/oauth?client_id=186645249405501&redirect_uri=https://www.itagency119.com/login&state=&response_type=token'
 }
